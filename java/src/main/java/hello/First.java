1815e0845a6c3277b7c4eaff73053501bda6e53e
Nysio Fontes Chrysostomo <nfontech@usersad.everis.int>
2017-01-16 15:21:11 -0200

    Codigo extremamente simples que representa um BackEnd, com os dados para continuar o tutorial em angular, e finalizar o projeto de consulta

---
+++ java/src/main/java/hello/First.java
@@ -0,0 +1,26 @@
+package hello;
+
+public class First {
+	
+	private Long id;
+	private String content;
+	
+	public First(Long id, String content){
+		this.id = id;
+		this.content = content;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
